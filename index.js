1fb5eb274eca38b976e2a4d9df3d372dc3624cf5
Alphabeee <108117846+Alphabeee@users.noreply.github.com>
2023-01-14 14:18:22 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const { REST, SlashCommandBuilder, Routes , EmbedBuilder, messageLink} = require('discord.js');
 const { Client, GatewayIntentBits } = require('discord.js');
+const { channel } = require('node:diagnostics_channel');
 const { token,botid } = require('./token.json');
+const wait = require('node:timers/promises').setTimeout;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -8,6 +10,8 @@ const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new SlashCommandBuilder().setName('delay').setDescription('Replies with ping(ms)!'),
+    new SlashCommandBuilder().setName('dice').setDescription('dice')
 ]
 	.map(command => command.toJSON());
 
@@ -28,10 +32,27 @@ rest.put(Routes.applicationCommands(botid), { body: commands })
     
         if (commandName === 'ping') {
             await interaction.reply('Pong!');
+            await wait(2000);
+            await interaction.editReply('Pong again!');
         } else if (commandName === 'server') {
             await interaction.reply(`伺服器名稱: ${interaction.guild.name}\n人員總數: ${interaction.guild.memberCount}`);
         } else if (commandName === 'user') {
             await interaction.reply(`你的名字為: ${interaction.user.tag}\n你的id為: ${interaction.user.id}`);
+        }
+        else if(commandName === 'delay'){
+            const msg = await interaction.reply({
+                content: "calculating delay...",
+                fetchReply: true
+            });
+            const ping=msg.createdTimestamp-interaction.createdTimestamp;
+            await interaction.editReply(`ping(ms) is ${ping}\n API ${client.ws.ping}`)
+        }
+        else if(commandName === 'dice'){
+            const final = Math.floor(Math.random() * (6 - 1))+1
+            const diceEmbed = new EmbedBuilder()
+                .setColor("#5865F2")
+                .setTitle(`🎲你得到了 ${final}`)
+            interaction.channel.send({embeds:[diceEmbed]});
         }
     });
     
